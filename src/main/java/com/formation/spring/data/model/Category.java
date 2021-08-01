326111f3bd10759a2f5e4db5736e8d903cca3e21
Mohamed-Sellemi <mohammed.sellemi@gmail.com>
2021-08-01 10:52:25 +0100

    créer les entities dans le package model

---
+++ src/main/java/com/formation/spring/data/model/Category.java
@@ -0,0 +1,41 @@
+package com.formation.spring.data.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categorie")
+public final class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "categorie_id")
+    private int categoryId;
+    @Column(name = "nom")
+    private String name;
+    
+    public int getCategoryId() {
+        return categoryId;
+    }
+    
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return "Category [categoryId=" + categoryId + ", name=" + name + "]";
+    }
+    
+}
---
+++ src/main/java/com/formation/spring/data/model/Comment.java
@@ -0,0 +1,42 @@
+package com.formation.spring.data.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "commentaire")
+public final class Comment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "commentaire_id")
+    private int commentId;
+    @Column(name = "contenu")
+    private String content;
+    
+    public int getCommentId() {
+        return commentId;
+    }
+    
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+    
+    public String getContent() {
+        return content;
+    }
+    
+    public void setContent(String content) {
+        this.content = content;
+    }
+    
+    @Override
+    public String toString() {
+        return "Comment [commentId=" + commentId + ", content=" + content + "]";
+    }
+    
+}
